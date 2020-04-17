b5ac88a354da4375c0452702b7d5c300b5568edc
Pawel Haladyj <pawel@haladyj.pl>
2020-04-17 16:22:12 +0200

    Multiple Authentication Providers Ss6 init

---
+++ src/main/java/pl/haladyj/springsecurity6/SpringSecurity6Application.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/haladyj/springsecurity6/SpringSecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
